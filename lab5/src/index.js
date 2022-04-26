b115806630b1e11d10e22424287c2fb14bf227d7
Dmitriy Naumov <naumov1024@gmail.com>
2022-04-26 20:37:55 +0300

    + lab 5

---
+++ lab5/src/index.js
@@ -0,0 +1,10 @@
+import "./css/normalize.css"
+import "./css/style.css"
+import "./css/small-screen.css"
+import "./css/medium-screen.css"
+import "./css/big-screen.css"
+import "./css/huge-screen.css"
+
+document.body.onload = function () {
+    console.log("Hello!");
+}
---
+++ lab5/webpack.config.js
@@ -0,0 +1,49 @@
+// webpack.config.js 
+const path = require('path') 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {  
+    entry: { 
+        main: path.resolve(__dirname, './src/index.js'),
+    }, 
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js', 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/pages/index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/news.html',
+            filename: 'news.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/photo.html',
+            filename: 'photo.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/timetable.html',
+            filename: 'timetable.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './src/pages/img', to: './img' }
+            ]
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    }
+}
